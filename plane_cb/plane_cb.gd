8d3c9626b2e1c9c3b00714ba74c3a42cc3f6f828
David <d.weckerly@gmail.com>
2023-09-21 11:07:52 -0500

    pipes with lasers scene

--- plane_cb/plane_cb.gd
+++ plane_cb/plane_cb.gd
@@ -1,14 +1,26 @@
 extends CharacterBody2D
 
+@onready var animation_player = $AnimationPlayer
+@onready var animated_sprite = $AnimatedSprite2D
+
 const GRAVITY: float = 1900.0
 const POWER: float = -400.0
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
 	velocity.y += GRAVITY * delta
+	fly()
+	move_and_slide()
+	if is_on_floor():
+		die()
+
+func fly() -> void:
 	if Input.is_action_just_pressed("fly"):
 		velocity.y = POWER
-	move_and_slide()
+		animation_player.play("fly")
+
+func die() -> void:
+	animated_sprite.stop()
+	set_physics_process(false)
