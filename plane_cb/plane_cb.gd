588aa6679a84e955f7fb83f5fc695e4a62c0744c
David <d.weckerly@gmail.com>
2023-09-21 10:48:12 -0500

    plane controls

--- plane_cb/plane_cb.gd
+++ plane_cb/plane_cb.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 
+const GRAVITY: float = 1900.0
+const POWER: float = -400.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _physics_process(delta):
+	velocity.y += GRAVITY * delta
+	if Input.is_action_just_pressed("fly"):
+		velocity.y = POWER
+	move_and_slide()
